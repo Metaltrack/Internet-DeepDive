9e178e84ba61c5dde536be90cb52fdd2054c47bb
Metaltrack <arjunchandgude61@gmail.com>
2026-02-08 20:51:03 +0530

    Ze Resource

---
+++ git-game/Characters/player.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+@export var speed :float= 8000.0
+@export var card_array :Array = []
+@export var player_stats :Stats
+@export var health :int = 100
+
+func _physics_process(delta: float) -> void:
+	
+	var dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	velocity = dir * speed * delta
+	move_and_slide()
+	
---
+++ git-game/Resources/Stats.gd
@@ -0,0 +1,10 @@
+extends Resource
+
+class_name Stats
+@export var healh_restore :int = 25
+@export var action_count :int = 2
+@export var cool_down :int = 5
+@export var damage :int = 20
+@export var is_poisioned :bool = false
+@export var poison_damage :int = 5
+@export var have_poision :bool = false
